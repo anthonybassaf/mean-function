46968834c3efc6bddd2048a1cb6be1ef67b58629
anthonybassaf <60676144+anthonybassaf@users.noreply.github.com>
2020-06-25 01:31:06 +0300

    Fixes to last commit

--- mean.c
+++ mean.c
@@ -5,75 +5,64 @@
 
 int main(int argc, char const *argv[])
 {
+    int MeanType;
+    double a; 
+    double b;
 
-int MeanType;
-double a; 
-double b;
+    while (1){
 
-printf("1-Arithmetic Mean\n");
-printf("2-Geometric Mean\n");
-printf("3-Harmonic Mean\n");
-printf("4-Exit\n");
+        printf("1-Arithmetic Mean\n");
+        printf("2-Geometric Mean\n");
+        printf("3-Harmonic Mean\n");
+        printf("4-Exit\n");
 
-printf("Choose the type of mean you want to calculate: ");
-scanf("%d", &MeanType);
+        printf("Choose the type of mean you want to calculate: ");
+        scanf("%d", &MeanType);
 
-while (MeanType != 4){
+        switch (MeanType) 
+        {
 
+            double mean;
 
-    switch (MeanType) 
-    {
+            case 0:
+            case 1:
+                printf("Choose a= ");
+                scanf("%lf", &a);
+                printf("Choose b= ");
+                scanf("%lf", &b);
 
-        double mean;
+                mean = (a + b)/2;
+                printf("The Arthmetic Mean of %.2f and %.2f is %.2f\n\n", a, b, mean);
 
-    case 0:
-    case 1:
+                break; 
 
-            printf("Choose a= ");
-            scanf("%lf", &a);
-            printf("Choose b= ");
-            scanf("%lf", &b);
+            case 2:
+                printf("Choose a= ");
+                scanf("%lf", &a);
+                printf("Choose b= ");
+                scanf("%lf", &b);
 
-            mean = (a + b)/2;
-            printf("The Arthmetic Mean of %f and %f is %f", a, b, mean);
+                mean = pow((a * b),(0.5));
+                printf("The Geometric Mean of %.2f and %.2f is %.2f\n\n", a, b, mean);
+                break;
+            
+            case 3:
+                printf("Choose a= ");
+                scanf("%lf", &a);
+                printf("Choose b= ");
+                scanf("%lf", &b);
 
-            break; 
+                mean = (a * b)/(a + b);
+                printf("The Harmonic Mean of %.2f and %.2f is %.2f\n\n", a, b, mean);
+                break;
 
-    case 2:
+            case 4:
+                printf("Goodbye!\n\n");
+                return 0;
 
-        printf("Choose a= ");
-        scanf("%lf", &a);
-        printf("Choose b= ");
-        scanf("%lf", &b);
-
-            mean = pow((a * b),(0.5));
-            printf("The Geometric Mean of %f and %f is %f", a, b, mean);
-        break;
-        
-    case 3:
-
-        printf("Choose a= ");
-        scanf("%lf", &a);
-        printf("Choose b= ");
-        scanf("%lf", &b);
-
-            mean = (a * b)/(a + b);
-            printf("The Harmonic Mean of %f and %f is %f", a, b, mean);
-        break;
-
-    case 4:
-        printf("Goodbye!\n");
-        break;
-
-
-    default:
-    printf("Choose a number between 1 and 4\n");
-    break;
-
+            default:
+                printf("Choose a number between 1 and 4\n\n");
+                break;
+        }
     }
-
-}   
-
-    return 0;
-
 }
